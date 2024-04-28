26f5e579b6cf3c1313f2c5fd7d6c527f190ca582
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-28 17:18:47 +0530

    Added QueryClientProvider around <App/>

--- starter/src/main.jsx
+++ starter/src/main.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const htmlElement = document.getElementById("root");
+const root = ReactDOM.createRoot(htmlElement);
+root.render(
+  <QueryClientProvider client={QueryClient}>
+    <App />
+  </QueryClientProvider>
+);
