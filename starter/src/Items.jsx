633a46bd129fb5686fe253a8bed2bacb3db32d6d
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-28 18:09:32 +0530

    Successfully made an API call to the api using react query

--- starter/src/Items.jsx
+++ starter/src/Items.jsx
@@ -1,9 +1,22 @@
-import SingleItem from './SingleItem';
+import { useQuery } from "@tanstack/react-query";
+import SingleItem from "./SingleItem";
+import customFetch from "./utils";
 const Items = ({ items }) => {
+  const result = useQuery({
+    queryKey: ["tasks"],
+    queryFn: () => customFetch.get("/"),
+  });
+
+  console.log(result);
   return (
-    <div className='items'>
+    <div className="items">
       {items.map((item) => {
-        return <SingleItem key={item.id} item={item} />;
+        return (
+          <SingleItem
+            key={item.id}
+            item={item}
+          />
+        );
       })}
     </div>
   );
