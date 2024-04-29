d510fc1bae6fc0283117454f7f28e427ff5ff327
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-29 15:06:05 +0530

    edit tasks on the server --- reactQuery is fantastic

--- starter/src/SingleItem.jsx
+++ starter/src/SingleItem.jsx
@@ -1,23 +1,40 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import customFetch from "./utils";
+import { useState } from "react";
+
 const SingleItem = ({ item }) => {
+  // Real Time update we'll go with the useQueryClient()
+  const queryClient = useQueryClient();
+
+  const { mutate: editTask, isLoading } = useMutation({
+    mutationFn: ({ taskId, isDone }) => {
+      return customFetch.patch(`${taskId}`, { isDone });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["tasks"]);
+    },
+  });
+
   return (
-    <div className='single-item'>
+    <div className="single-item">
       <input
-        type='checkbox'
+        type="checkbox"
         checked={item.isDone}
-        onChange={() => console.log('edit task')}
+        onChange={(e) => editTask({ taskId: item.id, isDone: !item.isDone })}
+        disabled={isLoading}
       />
       <p
         style={{
-          textTransform: 'capitalize',
-          textDecoration: item.isDone && 'line-through',
+          textTransform: "capitalize",
+          textDecoration: item.isDone && "line-through",
         }}
       >
         {item.title}
       </p>
       <button
-        className='btn remove-btn'
-        type='button'
-        onClick={() => console.log('delete task')}
+        className="btn remove-btn"
+        type="button"
+        onClick={() => console.log("delete task")}
       >
         delete
       </button>
