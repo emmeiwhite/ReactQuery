188b68cf30eb5fc58de6e4c8644461e330e3751e
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-29 14:30:43 +0530

    onSuccess:()=>{} Synced and runtime updates of tasks... Fantastic

--- starter/src/Form.jsx
+++ starter/src/Form.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 import customFetch from "./utils";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState("");
 
+  const queryClient = useQueryClient();
   const { mutate: createTask, isLoading } = useMutation({
     mutationFn: (task) => customFetch.post("/", { title: task }),
     onError: (error) => {
       toast.error(error.response.data.msg);
     },
     onSuccess: () => {
-      setNewItemName("");
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+
       toast.success("Task Added Successfully!");
+      setNewItemName("");
     },
   });
 
